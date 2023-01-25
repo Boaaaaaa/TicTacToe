55d6532d2b3ddd31a9a4c25c20e42cf555902233
Boaaaaaa <qhdk15@gmail.com>
2023-01-25 17:51:11 -0500

    first commit

---
+++ TicTacToe/TicTacToeFrom.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace BImAssignment1
+{
+    partial class TicTacToeFrom
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.A1 = new System.Windows.Forms.PictureBox();
+            this.A2 = new System.Windows.Forms.PictureBox();
+            this.A3 = new System.Windows.Forms.PictureBox();
+            this.B3 = new System.Windows.Forms.PictureBox();
+            this.B2 = new System.Windows.Forms.PictureBox();
+            this.B1 = new System.Windows.Forms.PictureBox();
+            this.C1 = new System.Windows.Forms.PictureBox();
+            this.C2 = new System.Windows.Forms.PictureBox();
+            this.C3 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.A1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.A2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.A3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.B3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.B2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.B1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.C1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.C2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.C3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // A1
+            // 
+            this.A1.BackColor = System.Drawing.Color.White;
+            this.A1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.A1.Location = new System.Drawing.Point(59, 106);
+            this.A1.Name = "A1";
+            this.A1.Size = new System.Drawing.Size(100, 100);
+            this.A1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.A1.TabIndex = 0;
+            this.A1.TabStop = false;
+            this.A1.Click += new System.EventHandler(this.pbClick);
+            // 
+            // A2
+            // 
+            this.A2.BackColor = System.Drawing.Color.White;
+            this.A2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.A2.Location = new System.Drawing.Point(184, 106);
+            this.A2.Name = "A2";
+            this.A2.Size = new System.Drawing.Size(100, 100);
+            this.A2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.A2.TabIndex = 0;
+            this.A2.TabStop = false;
+            this.A2.Click += new System.EventHandler(this.pbClick);
+            // 
+            // A3
+            // 
+            this.A3.BackColor = System.Drawing.Color.White;
+            this.A3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.A3.Location = new System.Drawing.Point(305, 106);
+            this.A3.Name = "A3";
+            this.A3.Size = new System.Drawing.Size(100, 100);
+            this.A3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.A3.TabIndex = 0;
+            this.A3.TabStop = false;
+            this.A3.Click += new System.EventHandler(this.pbClick);
+            // 
+            // B3
+            // 
+            this.B3.BackColor = System.Drawing.Color.White;
+            this.B3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.B3.Location = new System.Drawing.Point(305, 227);
+            this.B3.Name = "B3";
+            this.B3.Size = new System.Drawing.Size(100, 100);
+            this.B3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.B3.TabIndex = 0;
+            this.B3.TabStop = false;
+            this.B3.Click += new System.EventHandler(this.pbClick);
+            // 
+            // B2
+            // 
+            this.B2.BackColor = System.Drawing.Color.White;
+            this.B2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.B2.Location = new System.Drawing.Point(184, 227);
+            this.B2.Name = "B2";
+            this.B2.Size = new System.Drawing.Size(100, 100);
+            this.B2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.B2.TabIndex = 0;
+            this.B2.TabStop = false;
+            this.B2.Click += new System.EventHandler(this.pbClick);
+            // 
+            // B1
+            // 
+            this.B1.BackColor = System.Drawing.Color.White;
+            this.B1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.B1.Location = new System.Drawing.Point(59, 227);
+            this.B1.Name = "B1";
+            this.B1.Size = new System.Drawing.Size(100, 100);
+            this.B1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.B1.TabIndex = 0;
+            this.B1.TabStop = false;
+            this.B1.Click += new System.EventHandler(this.pbClick);
+            // 
+            // C1
+            // 
+            this.C1.BackColor = System.Drawing.Color.White;
+            this.C1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.C1.Location = new System.Drawing.Point(59, 349);
+            this.C1.Name = "C1";
+            this.C1.Size = new System.Drawing.Size(100, 100);
+            this.C1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.C1.TabIndex = 0;
+            this.C1.TabStop = false;
+            this.C1.Click += new System.EventHandler(this.pbClick);
+            // 
+            // C2
+            // 
+            this.C2.BackColor = System.Drawing.Color.White;
+            this.C2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.C2.Location = new System.Drawing.Point(184, 349);
+            this.C2.Name = "C2";
+            this.C2.Size = new System.Drawing.Size(100, 100);
+            this.C2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.C2.TabIndex = 0;
+            this.C2.TabStop = false;
+            this.C2.Click += new System.EventHandler(this.pbClick);
+            // 
+            // C3
+            // 
+            this.C3.BackColor = System.Drawing.Color.White;
+            this.C3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.C3.Location = new System.Drawing.Point(305, 349);
+            this.C3.Name = "C3";
+            this.C3.Size = new System.Drawing.Size(100, 100);
+            this.C3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.C3.TabIndex = 0;
+            this.C3.TabStop = false;
+            this.C3.Click += new System.EventHandler(this.pbClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Kristen ITC", 40.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.label1.ForeColor = System.Drawing.Color.Pink;
+            this.label1.Location = new System.Drawing.Point(46, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(369, 74);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Tic Tac Toe";
+            // 
+            // TicTacToeFrom
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(464, 473);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.C3);
+            this.Controls.Add(this.C2);
+            this.Controls.Add(this.C1);
+            this.Controls.Add(this.B1);
+            this.Controls.Add(this.B2);
+            this.Controls.Add(this.B3);
+            this.Controls.Add(this.A3);
+            this.Controls.Add(this.A2);
+            this.Controls.Add(this.A1);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Name = "TicTacToeFrom";
+            this.Text = "TicTacToe";
+            ((System.ComponentModel.ISupportInitialize)(this.A1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.A2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.A3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.B3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.B2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.B1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.C1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.C2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.C3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox A1;
+        private System.Windows.Forms.PictureBox A2;
+        private System.Windows.Forms.PictureBox A3;
+        private System.Windows.Forms.PictureBox B3;
+        private System.Windows.Forms.PictureBox B2;
+        private System.Windows.Forms.PictureBox B1;
+        private System.Windows.Forms.PictureBox C1;
+        private System.Windows.Forms.PictureBox C2;
+        private System.Windows.Forms.PictureBox C3;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ TicTacToe/TicTacToeFrom.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BImAssignment1
+{
+    public partial class TicTacToeFrom : Form
+    {
+        // Taking pictures from Properties
+        Image O = BImAssignment1.Properties.Resources.o;
+        Image X = BImAssignment1.Properties.Resources.x;
+
+        // Checking turn
+        bool Turn = true;
+        // It helps to count of turn to 9
+        int TurnCount = 0;
+        // for checking winneer
+        bool winner = false;
+
+        public TicTacToeFrom()
+        {
+            InitializeComponent();
+        }
+
+        // This method is called when user click picture box
+        private void pbClick(object sender, EventArgs e)
+        {
+            // Casting sender's data type to PictureBox data type
+            PictureBox pictureBox = (PictureBox)sender;
+
+            // This statement help to start with 'X'
+            if (Turn)
+            {
+                pictureBox.Image = X;
+            }
+            else
+            {
+                pictureBox.Image = O;
+            }
+
+            Turn = !Turn;
+            TurnCount++;
+            // selected picturebox cannot be selected again
+            pictureBox.Enabled = false;
+
+            // Everytime user click one picturebox checks whether there is a winner
+            Winner();
+
+            // If there is a winner, this statement makes this game to initialize
+            if (winner || (!winner && TurnCount == 9))
+                Initialize();
+        }
+
+        private void Winner()
+        {
+            // Check winner by hoizontal line
+            if(A1.Image == A2.Image && A2.Image == A3.Image && !A1.Enabled)
+            {
+                winner = true;
+            }
+            else if (B1.Image == B2.Image && B2.Image == B3.Image && !B1.Enabled)
+            {
+                winner = true;
+            }
+            else if (C1.Image == C2.Image && C2.Image == C3.Image && !C1.Enabled)
+            {
+                winner = true;
+            }
+
+            // Check winner by vertical line
+            else if (A1.Image == B1.Image && B1.Image == C1.Image && !A1.Enabled)
+            {
+                winner = true;
+            }
+            else if (A2.Image == B2.Image && B2.Image == C2.Image && !A2.Enabled)
+            {
+                winner = true;
+            }
+            else if (A3.Image == B3.Image && B3.Image == C3.Image && !A3.Enabled)
+            {
+                winner = true;
+            }
+
+            // Check winner by diagonal line
+            else if (A1.Image == B2.Image && B2.Image == C3.Image && !A1.Enabled)
+            {
+                winner = true;
+            }
+            else if (A3.Image == B2.Image && B2.Image == C1.Image && !A3.Enabled)
+            {
+                winner = true;
+            }
+
+            // Show messagebox whether there is a winner or not
+            if(winner)
+            {
+                if(Turn)
+                    MessageBox.Show("The winner is O");
+                else if (!Turn)
+                    MessageBox.Show("The winner is X");
+            }
+            else if(!winner && TurnCount == 9)
+            {
+                MessageBox.Show("Draw");
+            }
+
+        }
+
+        // This method is for initializing this game
+        private void Initialize()
+        {
+            // Remove image
+            A1.Image = null;
+            A2.Image = null;
+            A3.Image = null;
+            B1.Image = null;
+            B2.Image = null;
+            B3.Image = null;
+            C1.Image = null;
+            C2.Image = null;
+            C3.Image = null;
+
+            // Enable picture boxes
+            A1.Enabled = true;
+            A2.Enabled = true;
+            A3.Enabled = true;
+            B1.Enabled = true;
+            B2.Enabled = true;
+            B3.Enabled = true;
+            C1.Enabled = true;
+            C2.Enabled = true;
+            C3.Enabled = true;
+
+            // Initializing variables
+            Turn = true;
+            TurnCount = 0;
+            winner = false;
+        }
+    }
+}
